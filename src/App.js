9f87440917be661d6f1e06e47167f65d9d4dbe70
Nitindev07 <nitin.dev2907@gmail>
2024-01-26 18:15:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import Home from "./components/Home";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,10 @@ const Footer = () => {
     <div className='w-full bg-black'>
     <div className='flex px-5 py-8 max-w-[1360px] w-full mx-auto justify-between items-center'>
       <h5 className='text-[13px]'>All right reversed, 2024</h5>
-      <div className='flex gap-9 items-center text-gray-200 justify-center'>
-        <MdOutlineWhatsapp size={30}/>
-        <FaLinkedin  size={30}/>
-        <FaTwitter size={30}/>
+      <div className='flex gap-9 items-center text-gray-300 justify-center'>
+        <MdOutlineWhatsapp className='hover:text-white/50 cursor-pointer' size={25}/>
+        <FaLinkedin className='hover:text-white/50 cursor-pointer'  size={25}/>
+        <FaTwitter className='hover:text-white/50 cursor-pointer' size={25}/>
       </div>
     </div>
     </div>
